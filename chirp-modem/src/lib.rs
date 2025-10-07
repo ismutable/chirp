ee5e63307c8f96d091efac48ac18d111e1dd02bc
Daniel Castelli <daniel@mutable.blog>
2025-10-06 22:27:58 -0700

    WaveGenerator Iterator trait implements but untested.

--- chirp-modem/src/lib.rs
+++ chirp-modem/src/lib.rs
@@ -1,22 +1,22 @@
 use std::f32::consts::TAU;
 use std::sync::LazyLock;
 
-use bitvec::order::Lsb0;
-use bitvec::slice::BitSlice;
-use bitvec::slice::Iter as BitIter;
-use bitvec::view::AsBits;
-use bitvec::view::BitView;
+use bitvec::{
+    order::Lsb0,
+    slice::{BitSlice, Iter as BitIter},
+    view::BitView,
+};
 
 pub type Hz = u16;
 
 pub const BIT_REPEATS: u8 = 16;
-pub const CARRIER_STEPS: usize = 13;
-pub const CARRIER_SAMPLES: usize = 32;
+pub const CARRIER_STEPS: u8 = 13;
+pub const CARRIER_SAMPLES: u8 = 32;
 pub const CARRIER_FREQ: Hz = 19_500;
 pub const SAMPLE_RATE: Hz = 48_000;
 
 pub static CARRIER_SIGNAL: LazyLock<Vec<i16>> = LazyLock::new(|| {
-    let mut carrier = Vec::with_capacity(CARRIER_SAMPLES);
+    let mut carrier = Vec::with_capacity(CARRIER_SAMPLES as usize);
     for i in 0..CARRIER_SAMPLES {
         let radian = ((i as f32) / (CARRIER_SAMPLES as f32)) * TAU;
         let scaled = radian.sin() * (i16::MAX as f32);
@@ -27,8 +27,11 @@ pub static CARRIER_SIGNAL: LazyLock<Vec<i16>> = LazyLock::new(|| {
 });
 
 pub struct WaveGenerator<'a> {
+    one: i16,
+    zero: i16,
     cursor: u8,
     count: u8,
+    hold: bool,
     bits: BitIter<'a, u8, Lsb0>,
 }
 
@@ -36,23 +39,32 @@ impl<'a> WaveGenerator<'a> {
     fn new(data: &'a [u8]) -> Self {
         let bits: &'a BitSlice<u8, Lsb0> = data.view_bits::<Lsb0>();
         Self {
+            one: i16::MAX,
+            zero: i16::MAX / 10,
             cursor: 0,
             count: 0,
+            hold: false,
             bits: bits.iter(),
         }
     }
 }
 
-// impl<'a> Iterator for WaveGenerator<'a> {
-//     type Item = i16;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if self.iter.is_none() {
-//             self.iter = Some(self.data.as_bits().into_iter());
-//         }
-//         Some(0)
-//     }
-// }
+impl<'a> Iterator for WaveGenerator<'a> {
+    type Item = i16;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count == 0 {
+            self.hold = *self.bits.next()?;
+        }
+        let value = CARRIER_SIGNAL[self.cursor as usize];
+        self.count = (self.count + 1) % BIT_REPEATS;
+        self.cursor = (self.cursor + CARRIER_STEPS) % CARRIER_SAMPLES;
+        if self.hold {
+            Some(self.one * value)
+        } else {
+            Some(self.zero * value)
+        }
+    }
+}
 
 pub struct WaveBuilder {}
 
