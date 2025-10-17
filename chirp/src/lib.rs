a15d1446258159f29abf9e112f8cf3a22c2e1247
Daniel Castelli <daniel@mutable.blog>
2025-10-16 20:17:51 -0700

    creating bit modulator chain api

--- chirp/src/lib.rs
+++ chirp/src/lib.rs
@@ -1,28 +1,39 @@
 use std::f32::consts::TAU;
 
-const SAMPLES: usize = 32;
-const CURSOR_STEP: usize = 13;
+const FRAME: usize = 32;
+const STEP: usize = 13;
+const SAMPLES: usize = FRAME;
 const DEFAULT_HI_GAIN: f32 = 0.9;
 const DEFAULT_LO_GAIN: f32 = 0.1;
 
 pub struct BitModulator {
-    unit: [f32; SAMPLES],
-    hi: [f32; SAMPLES],
-    lo: [f32; SAMPLES],
+    unit: [f32; FRAME],
+    hi: [f32; FRAME],
+    lo: [f32; FRAME],
+    bit: bool,
+    cursor: usize,
+    sample: usize,
 }
 
 impl Default for BitModulator {
     fn default() -> Self {
-        let mut unit = [0.0; SAMPLES];
-        let mut hi = [0.0; SAMPLES];
-        let mut lo = [0.0; SAMPLES];
-        let phase_step = TAU / SAMPLES as f32;
-        for (idx, sample) in unit.iter_mut().enumerate() {
-            *sample = f32::sin(idx as f32 * phase_step);
-            hi[idx] = DEFAULT_HI_GAIN * (*sample);
-            lo[idx] = DEFAULT_LO_GAIN * (*sample);
+        let mut unit = [0.0; FRAME];
+        let mut hi = [0.0; FRAME];
+        let mut lo = [0.0; FRAME];
+        let phase_step = TAU / FRAME as f32;
+        for (idx, val) in unit.iter_mut().enumerate() {
+            *val = f32::sin(idx as f32 * phase_step);
+            hi[idx] = DEFAULT_HI_GAIN * (*val);
+            lo[idx] = DEFAULT_LO_GAIN * (*val);
         }
-        Self { unit, hi, lo }
+        Self {
+            unit,
+            hi,
+            lo,
+            bit: false,
+            cursor: 0,
+            sample: 0,
+        }
     }
 }
 
@@ -31,6 +42,18 @@ impl BitModulator {
         for (s, u) in self.hi.iter_mut().zip(self.unit.iter()) {
             *s = gain * u;
         }
+        self
+    }
+    pub fn lo_gain(&mut self, gain: f32) -> &mut Self {
+        for (s, u) in self.lo.iter_mut().zip(self.unit.iter()) {
+            *s = gain * u;
+        }
+        self
+    }
+    pub fn bit(&mut self, bit: bool) -> &mut Self {
+        self.bit = bit;
+        self.cursor = 0;
+        self.sample = 0;
         self
     }
 }
