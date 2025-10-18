62d59b61cd95b1ab2cf78bc9142617ebc13eb609
Daniel Castelli <daniel@mutable.blog>
2025-10-17 21:43:01 -0700

    converting to continous chunks for more efficient copying

--- chirp/src/lib.rs
+++ chirp/src/lib.rs
@@ -2,100 +2,103 @@ use std::f32::consts::TAU;
 
 const FRAME: usize = 32;
 const STEP: usize = 13;
-const SAMPLES: usize = FRAME;
-const DEFAULT_HI_GAIN: f32 = 0.9;
-const DEFAULT_LO_GAIN: f32 = 0.1;
+const HI_GAIN: f32 = 1.0;
+const LO_GAIN: f32 = 0.1;
 
 #[derive(Debug, Clone)]
 pub struct BitModulator {
-    unit: [f32; FRAME],
     hi: [f32; FRAME],
     lo: [f32; FRAME],
-    bit: bool,
-    cursor: usize,
-    sample: usize,
 }
 
 impl Default for BitModulator {
     fn default() -> Self {
-        let mut unit = [0.0; FRAME];
         let mut hi = [0.0; FRAME];
         let mut lo = [0.0; FRAME];
-        let phase_step = TAU / FRAME as f32;
-        for (idx, sample) in unit.iter_mut().enumerate() {
-            *sample = f32::sin(idx as f32 * phase_step);
-            hi[idx] = DEFAULT_HI_GAIN * (*sample);
-            lo[idx] = DEFAULT_LO_GAIN * (*sample);
+        let phase_step = (TAU / FRAME as f32) * STEP as f32;
+        for idx in 0..FRAME {
+            let sample = f32::sin(idx as f32 * phase_step);
+            hi[idx] = HI_GAIN * sample;
+            lo[idx] = LO_GAIN * sample;
         }
-        Self {
-            unit,
-            hi,
-            lo,
-            bit: false,
-            cursor: 0,
-            sample: 0,
-        }
+        Self { hi, lo }
     }
 }
 
 impl BitModulator {
-    pub fn new(bit: bool) -> Self {
-        Self {
-            bit,
-            ..Default::default()
+    pub fn signal(&self, bit: bool) -> &[f32] {
+        if bit {
+            &self.hi
+        } else {
+            &self.lo
         }
-    }
-
-    pub fn hi_gain(mut self, gain: f32) -> Self {
-        for (s, u) in self.hi.iter_mut().zip(self.unit.iter()) {
-            *s = gain * u;
-        }
-        self
-    }
-    pub fn lo_gain(mut self, gain: f32) -> Self {
-        for (s, u) in self.lo.iter_mut().zip(self.unit.iter()) {
-            *s = gain * u;
-        }
-        self
-    }
-    pub fn bit(mut self, bit: bool) -> Self {
-        self.bit = bit;
-        self.cursor = 0;
-        self.sample = 0;
-        self
     }
 }
 
-impl Iterator for BitModulator {
-    type Item = f32;
+struct WaveReader<'a> {
+    data: &'a [f32],
+    pos: usize,
+}
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.sample == SAMPLES {
-            return None;
-        }
+impl<'a> WaveReader<'a> {
+    pub fn remaining(&self) -> usize {
+        self.data.len() - self.pos
+    }
 
-        let lookup = if self.bit {
-            self.hi.as_slice()
+    pub fn read(&mut self, size: usize) -> &[f32] {
+        if size > self.remaining() {
+            let output = &self.data[self.pos..];
+            self.pos = self.data.len();
+            output
         } else {
-            self.lo.as_slice()
+            let stop = self.pos + size;
+            let output = &self.data[self.pos..stop];
+            self.pos = stop;
+            output
+        }
+    }
+}
+
+impl<'a> From<&'a [f32]> for WaveReader<'a> {
+    fn from(data: &'a [f32]) -> Self {
+        WaveReader { data, pos: 0 }
+    }
+}
+
+struct WaveWriter<'a> {
+    buffer: &'a mut [f32],
+    pos: usize,
+}
+
+impl<'a> From<&'a mut [f32]> for WaveWriter<'a> {
+    fn from(buffer: &'a mut [f32]) -> Self {
+        WaveWriter { buffer, pos: 0 }
+    }
+}
+
+enum Status {
+    Full,
+    Remaining(usize),
+}
+
+impl<'a> WaveWriter<'a> {
+    pub fn remaining(&self) -> usize {
+        self.buffer.len() - self.pos
+    }
+
+    pub fn write(&mut self, reader: WaveReader) -> Status {
+        // handle reader empty case
+        if reader.remaining() == 0 {
+            return Status::Remaining(self.remaining());
         };
 
-        let sample = lookup[self.cursor];
-        self.cursor = (self.cursor + STEP) % FRAME;
-        self.sample += 1;
+        // handle buffer full case
+        if self.remaining() == 0 {
+            return Status::Full;
+        };
 
-        Some(sample)
-    }
-}
+        if self.remaining() > reader.remaining {}
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    /// test peak of sine curve is equal to hi gain
-    #[test]
-    fn hi_gain() {
-        const GAIN: f32 = 0.5;
-        assert_eq!(GAIN, BitModulator::new(true).hi_gain(GAIN).nth(8).unwrap());
+        Status::Remaining(self.remaining())
     }
 }
