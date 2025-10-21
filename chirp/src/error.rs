9a9f6ae07b6167d571f000b98a38ccb9e9af9ab4
Daniel Castelli <daniel@mutable.blog>
2025-10-20 21:45:21 -0700

    added message modulation and corresponding unit tests. next graph waveforms to make sure there are no unexpected phenomena, then implement cpal output and input in full duplex.

--- chirp/src/error.rs
+++ chirp/src/error.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 
+#[derive(Error, Debug)]
 pub enum ChirpError {
     #[error("Data requires buffer of {required} length, but only {available} available.")]
     Buffer { required: usize, available: usize },
--- chirp/src/message.rs
+++ chirp/src/message.rs
@@ -36,3 +36,49 @@ pub fn modulate(bytes: &[u8], buffer: &mut [f32]) -> Result<(), ChirpError> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn two_byte_message() {
+        let msg = [0, 1];
+        let mut buffer = [0.0; FRAME * 2 * 8];
+        let lookup = BitModulator::default();
+        let expected: Vec<f32> = [
+            // msg[0]
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            // msg[1]
+            lookup.modulate(true),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+            lookup.modulate(false),
+        ]
+        .into_iter()
+        .flat_map(|v| v.to_owned())
+        .collect();
+        modulate(&msg, &mut buffer).expect("Buffer length insufficient.");
+        assert_eq!(buffer.as_slice(), expected.as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_insufficient_buffer() {
+        let msg = [1, 2, 3];
+        let mut buffer = [0.0; FRAME];
+        modulate(&msg, &mut buffer).expect("Should panic, buffer not long enough.")
+    }
+}
