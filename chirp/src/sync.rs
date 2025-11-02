c0d23efe4d6e69614e603c0b59de44a6a6a394dd
Daniel Castelli <daniel@mutable.blog>
2025-11-02 05:54:26 -0800

    converting to rtrb impl

--- chirp/src/sync.rs
+++ chirp/src/sync.rs
@@ -1,1 +1,24 @@
+use rtrb::{Consumer, Producer, RingBuffer};
 
+pub struct WaveChannel {}
+
+impl WaveChannel {
+    pub fn with_capacity<T>(capacity: usize) -> (WaveWriter<T>, WaveReader<T>) {
+        let (producer, consumer) = RingBuffer::new(capacity);
+        (WaveWriter { producer }, WaveReader { consumer })
+    }
+}
+
+pub struct WaveWriter<T> {
+    producer: Producer<T>,
+}
+
+pub struct WaveReader<T> {
+    consumer: Consumer<T>,
+}
+
+impl <T> WaveWriter<T> {
+    pub fn write<samples: &[T]> {
+        todo!();
+    }
+}
